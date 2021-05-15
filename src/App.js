18c72960f09edd076a63d95cd6ca85ba29342d42
JoseIgnacioTapia <josignaciot@gmail.com>
2021-05-14 21:23:48 -0300

    Showing Appoinments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment, useState } from 'react';
 import Formulario from './components/Formulario'
+import Cita from './components/Cita'
 
 function App() {
 
@@ -26,7 +27,14 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            2
+            <h2>Administra tus Citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
+
           </div>
         </div>
       </div>
